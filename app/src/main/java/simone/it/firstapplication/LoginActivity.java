ec2a4f8b119cb7e54a0e88cccec253e79ea6f6bf
SimoneScifo <simone.scifo97@gmail.com>
2017-02-10 10:40:42 +0100

    FirstApplication

---
+++ app/src/main/java/simone/it/firstapplication/LoginActivity.java
@@ -0,0 +1,8 @@
+package simone.it.firstapplication;
+
+/**
+ * Created by Simone on 08/02/2017.
+ */
+
+public class LoginActivity {
+}
---
+++ app/src/main/java/simone/it/firstapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package simone.it.firstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Simone on 08/02/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
